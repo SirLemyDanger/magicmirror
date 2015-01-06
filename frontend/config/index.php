b75c9db72a337a1aa64423d972852ff8c932d497
Lemy Danger <git@lemydanger.eu>
2015-01-06 23:49:14 +0100

    init config site

---
+++ frontend/config/index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+     <title>Magic Mirror Config</title>
+           <link rel="stylesheet" type="text/css" href="css/main.css">
+           <script src="../js/jquery.js"></script>
+    </head>
+    <body>
+        <div id="body">
+			Menschen, Tiere, Sensationen!<br>
+			Geräteeinstellungen: WLAN, Standort<br>
+			Nutzer: Name, Geschlecht, Fotos<br>
+        </div>
+    </body>
+</html>
