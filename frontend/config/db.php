3f6b7e6d69e4b4eeb6b04b7f8e784dbcc315e9fe
Lemy Danger <git@lemydanger.eu>
2015-01-08 21:34:43 +0100

    create new user in db via php

--- frontend/config/db.php
+++ frontend/config/db.php
@@ -1,4 +1,18 @@
 <?php
+function newUser($firstname,$lastname,$nickname,$sex,$birthday) {
+    $id = uniqid();
+    if ($nickname == "") {
+        $nickname = $firstname;
+    }
+    if ($sex != ("male"||"female")){
+        $sex = "";
+    }
+    $request = "INSERT INTO user
+    (id, firstname, lastname, nickname, sex, birthday)
+    VALUES
+    ('$id','$firstname','$lastname','$nickname','$sex','$birthday')";
+    return mysql_query($request);
+}
 $verbindung = mysql_connect ("localhost",
 "mirror", "raspberry")
 or die ("keine Verbindung möglich.
@@ -11,7 +25,7 @@ $abfrage = "SELECT firstname FROM user";
 $query = mysql_query($abfrage);
 if(!$query){
 	echo mysql_error();
-	}
+}
 else{   
         echo $query;
 	while($row = mysql_fetch_object($query))
@@ -19,7 +33,15 @@ else{
 		echo "$row->firstname <br>";
 	}
 	echo "toll";
-	}
-
+}
+$method = filter_input(INPUT_POST, "method");
+if ($method == "newuser"){
+    $firstname = filter_input(INPUT_POST, "firstname");
+    $lastname = filter_input(INPUT_POST, "lastname");
+    $nickname = filter_input(INPUT_POST, "nickname");
+    $firstname = filter_input(INPUT_POST, "sex");
+    $birthday = filter_input(INPUT_POST, "birthday");
+    newUser($firstname,$lastname,$nickname,$sex,$birthday);
+}
 
 ?>
