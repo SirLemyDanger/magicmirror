fe0168764ac0db060eebdbbe30e70e19c67c02c5
Lemy Danger <git@lemydanger.eu>
2015-01-08 19:48:40 +0100

    rumspielen mit db <->php

--- frontend/config/db.php
+++ frontend/config/db.php
@@ -8,16 +8,17 @@ mysql_select_db("magicmirror")
 or die ("Die Datenbank existiert nicht.");
 
 $abfrage = "SELECT firstname FROM user";
-$ergebnis = mysql_query($abfrage);
-$if(!query){
+$query = mysql_query($abfrage);
+$if(!$query){
 	echo mysql_error();
 	}
 else{
-	while($row = mysql_fetch_object($ergebnis))
+	while($row = mysql_fetch_object($query))
 	{
 		echo "$row->url <br>";
 	}
 	echo "toll";
 	}
+
 
 ?>
