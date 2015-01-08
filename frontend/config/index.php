d6f58cc3f6ea59d4fff197722b44b8da4d334956
Lemy Danger <git@lemydanger.eu>
2015-01-08 17:33:29 +0100

    config new user

--- frontend/config/index.php
+++ frontend/config/index.php
@@ -3,12 +3,24 @@
      <title>Magic Mirror Config</title>
            <link rel="stylesheet" type="text/css" href="css/main.css">
            <script src="../js/jquery.js"></script>
+		   <script src="js/main.js"></script>	
     </head>
     <body>
         <div id="body">
-			Menschen, Tiere, Sensationen!<br>
-			Geräteeinstellungen: WLAN, Standort<br>
-			Nutzer: Name, Geschlecht, Fotos<br>
+			<table>
+				<tr>
+					<td><a href="newuser.html">New User</a></td>
+					<td>Edit User</td>
+				</tr>
+				<tr>
+					<td>Upload Fotos</td>
+					<td>Device Options</td>
+				</tr>
+			</table>
+				Menschen, Tiere, Sensationen!<br>
+				Geräteeinstellungen: WLAN, Standort<br>
+				Nutzer: Name, Geschlecht, Fotos<br>
+			
         </div>
     </body>
 </html>
