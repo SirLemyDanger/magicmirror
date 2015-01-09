0dcbe7aaedd285806853a8f979c60a0b7ae8e24e
Lemy Danger <git@lemydanger.eu>
2015-01-09 12:57:45 +0100

    mysql error msg

--- frontend/config/db.php
+++ frontend/config/db.php
@@ -51,9 +51,28 @@ function deleteUser($id) {
     $query = "DELETE FROM user WHERE id = $id";
     return $mysqli->query($query);    
 }
-$mysqli->real_connect ("localhost",
-"mirror", "raspberry", "magicmirror")
-or die ("keine Verbindung zur Datenbank möglich.");
+//$mysqli->real_connect ("localhost",
+//"mirror", "raspberry", "magicmirror")
+//or die ("keine Verbindung zur Datenbank möglich.");
+if (!$mysqli) {
+    die('mysqli_init failed');
+}
+
+if (!$mysqli->options(MYSQLI_INIT_COMMAND, 'SET AUTOCOMMIT = 0')) {
+    die('Setting MYSQLI_INIT_COMMAND failed');
+}
+
+if (!$mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5)) {
+    die('Setting MYSQLI_OPT_CONNECT_TIMEOUT failed');
+}
+
+if (!$mysqli->real_connect ("localhost", "mirror", "raspberry", "magicmirror")) {
+    die('Connect Error (' . mysqli_connect_errno() . ') '
+            . mysqli_connect_error());
+}
+
+echo 'Success... ' . $mysqli->host_info . "\n";
+
 
 getAllUsers();
 $method = filter_input(INPUT_GET, "method");
