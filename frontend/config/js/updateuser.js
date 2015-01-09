94ff57c74af3f0833e17bb92d3ec0ce379c7748c
Lemy Danger <git@lemydanger.eu>
2015-01-09 15:45:31 +0100

    typo

--- frontend/config/js/updateuser.js
+++ frontend/config/js/updateuser.js
@@ -27,8 +27,11 @@ jQuery( document ).ready(function() {
 	getUserData.done(function(data){
 		$( "#firstname" ).attr( "value", data[0].firstname ).removeAttr("disabled");
 		$( "#lastname" ).attr( "value", data[0].lastname ).removeAttr("disabled");
-		if (data[0].sex == male){ $.( "#male" ).attr( checked="checked");
-		}else if(data[0].sex == female)	{$.( "#female" ).attr( checked="checked");}
+		if (data[0].sex == male){
+			$( "#male" ).attr( checked="checked");
+		}else if (data[0].sex == female){
+			$( "#female" ).attr( checked="checked");
+		}
 		$( "#sex" ).removeAttr("disabled");
 		$( "#birthday" ).attr( "value", data[0].birthday ).removeAttr("disabled");
 		$( "#submit" ).removeAttr("disabled");
