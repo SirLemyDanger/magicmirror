4cc4134e3666ea7b109964eb110f1f53eb0e7ffe
Lemy Danger <git@lemydanger.eu>
2015-01-10 13:29:22 +0100

    fix

--- frontend/config/db.php
+++ frontend/config/db.php
@@ -128,13 +128,13 @@ if ($method == "newuser"){
     $id = filter_input(INPUT_GET, "id");
     $answer = deleteuser($id);
 }
-echo $mysqli->info;
-if($answer == true)
-   {
-   echo "Database query succsessful";
-   }
-else
-   {
-   echo $answer->error;
-   }
+//echo $mysqli->info;
+//if($answer == true)
+//   {
+//   echo "Database query succsessful";
+//   }
+//else
+//   {
+//   echo $answer->error;
+//   }
 ?>
