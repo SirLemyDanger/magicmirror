6a3c63b40bc0dc92c19031611e3e41728b85eddc
Lemy Danger <git@lemydanger.eu>
2015-01-12 13:26:25 +0100

    userlist init

---
+++ frontend/config/js/user.js
@@ -0,0 +1,18 @@
+
+getUserData = $.ajax( {
+	url: "db.php",
+	async: true,
+	type: "GET",
+	dataType: "json",
+	data: {"method":"getallusers"}
+});
+jQuery( document ).ready(function() {
+	getUserData.done(function(data){
+		$( "#userlist" ).append( "
+			<tr id=\"data[0].id\">
+				<td class=\"firstname\">data[0].firstname</td> 
+				<td class=\"lastname\">data[0].lastname</td>
+				<td class=\"nickname\">data[0].nickname</td>
+			</tr>" );
+	});
+});
