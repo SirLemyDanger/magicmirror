2d6ddeeee8d77de9351d50b4557d87e4a8b5fd7b
Lemy Danger <git@lemydanger.eu>
2015-01-12 14:37:12 +0100

    radiobutton

--- frontend/config/js/updateuser.js
+++ frontend/config/js/updateuser.js
@@ -29,9 +29,9 @@ jQuery( document ).ready(function() {
 		$( "#firstname" ).attr( "value", data[0].firstname ).removeAttr("disabled");
 		$( "#lastname" ).attr( "value", data[0].lastname ).removeAttr("disabled");
 		$( "#nickname" ).attr( "value", data[0].nickname ).removeAttr("disabled");
-		if (data[0].sex == male){
+		if (data[0].sex == "male"){
 			$( "#male" ).attr( checked, "checked");
-		}else if (data[0].sex == female){
+		}else if (data[0].sex == "female"){
 			$( "#female" ).attr( checked, "checked");
 		//}else if (data[0].sex == other){
 		//	$( "#other" ).attr( checked, "checked");
