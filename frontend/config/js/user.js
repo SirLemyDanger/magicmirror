c6adaab97de0af55cea679390b0a014ab5e6ed47
Lemy Danger <git@lemydanger.eu>
2015-01-12 13:35:01 +0100

    js string

--- frontend/config/js/user.js
+++ frontend/config/js/user.js
@@ -8,11 +8,10 @@ getUserData = $.ajax( {
 });
 jQuery( document ).ready(function() {
 	getUserData.done(function(data){
-		$( "#userlist" ).append( "
-			<tr id=\"data[0].id\">
-				<td class=\"firstname\">data[0].firstname</td> 
-				<td class=\"lastname\">data[0].lastname</td>
-				<td class=\"nickname\">data[0].nickname</td>
-			</tr>" );
+		$( "#userlist" ).append( "<tr id="+data[0].id+"\">"+
+				"<td class=\"firstname\">"+data[0].firstname"</td>"+ 
+				"<td class=\"lastname\">"+data[0].lastname+"</td>"+
+				"<td class=\"nickname\">"+data[0].nickname+"</td>"+
+			"</tr>" );
 	});
 });
