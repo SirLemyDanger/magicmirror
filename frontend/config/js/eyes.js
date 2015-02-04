401c09e49350db7aece006abcd39250a74dbbedd
Lemy Danger <git@lemydanger.eu>
2015-02-04 21:30:56 +0100

    send eyes

--- frontend/config/js/eyes.js
+++ frontend/config/js/eyes.js
@@ -50,4 +50,28 @@ jQuery( document ).ready(function() {
 		righteye = false;
 		checkSendButton();
 	});
+	//callback handler for form submit
+$("#eyesform").submit(function(e)
+{
+    var postData = $(this).serializeArray();
+    var formURL = $(this).attr("action");
+    $.ajax(
+    {
+        url : formURL,
+        type: "POST",
+        data : postData,
+		dataType : "json",
+        success:function(data, textStatus, jqXHR)
+        {
+            //data: return data from server
+        },
+        error: function(jqXHR, textStatus, errorThrown)
+        {
+            //if fails     
+        }
+    });
+    e.preventDefault(); //STOP default action
+    e.unbind(); //unbind. to stop multiple form submit.
+});
+ 
 });
