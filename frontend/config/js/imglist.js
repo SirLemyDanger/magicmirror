8df9e707393d9ae45e730e04cf8eec5becede266
Lemy Danger <git@lemydanger.eu>
2015-02-05 12:33:21 +0100

    test

--- frontend/config/js/imglist.js
+++ frontend/config/js/imglist.js
@@ -14,17 +14,18 @@ getUserData = $.ajax( {
 	data: {"method":"getuserdata", "id": id }
 });
 jQuery( document ).ready(function() {
-	$("img.photolist").click(function(e) {
-		 //alert the clicked position.
-		 //alert(e.offsetX + ',' + e.offsetY);
-		 alert("hi");
-		});
+
 	getUserData.done(function(data){
 		$( "#name" ).html( "Photos for "+data[0].nickname+" ("+data[0].firstname+" "+data[0].lastname+")" );
 	});
 	getUserImageIds.done(function(data){
 		for(var i = 0, l = data.length; i < l; ++i){
 			$( "#body" ).append( '<img class="photolist" src="img.php?id='+ data[i].id +'" alt="photoid: '+ data[i].id +'">' );
+			$("img.photolist").click(function(e) {
+				 //alert the clicked position.
+				 //alert(e.offsetX + ',' + e.offsetY);
+				 alert("hi");
+			});
 		}
 		
 	});
