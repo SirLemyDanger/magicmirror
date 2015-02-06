90913b4e6f8356cc9ddad7ba30eb65c05058d800
Lemy Danger <git@lemydanger.eu>
2015-02-06 21:28:12 +0100

    fix

--- frontend/config/js/user.js
+++ frontend/config/js/user.js
@@ -23,7 +23,7 @@ jQuery( document ).ready(function() {
 	});	
 	getUserData.done(function(data){
 		for(var i = 0, l = data.length; i < l; ++i){
-			var userstring = "<tr id="+data[i].id+"\">\n"+
+			var userstring = "<tr id="+data[i].id+">\n"+
 								"<td class=\"firstname\">"+data[i].firstname+"</td>\n"+
 								"<td class=\"lastname\">"+data[i].lastname+"</td>\n"+
 								"<td class=\"nickname\">"+data[i].nickname+"</td>\n"+
