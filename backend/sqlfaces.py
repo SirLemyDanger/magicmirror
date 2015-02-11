2b2a447046924f0878291b26255990bf1aad05e9
Lemy Danger <git@lemydanger.eu>
2015-02-11 20:36:04 +0100

    img test

--- backend/sqlfaces.py
+++ backend/sqlfaces.py
@@ -6,17 +6,16 @@ import sqlconnection
 import cStringIO
 import newfaces
 
-raw_input("Press Return ")
 cnx = sqlconnection.connecttodb()
 
 cursor = cnx.cursor(dictionary=True)
-id = 3
+id = 17
 query = ("SELECT id, lefteye_x, lefteye_y, righteye_x, righteye_y, imgdata, imgtype FROM images WHERE id = (%s)")
 cursor.execute(query, (id,))
 for row in cursor:
 	file_like = cStringIO.StringIO(row["imgdata"])
 	image = Image.open(file_like)
-	#image = newfaces.CropFace( image, eye_left=(row["lefteye_x"],row["lefteye_y"]), eye_right=(row["righteye_x"],row["righteye_y"]))
+	image = newfaces.CropFace( image, eye_left=(row["lefteye_x"],row["lefteye_y"]), eye_right=(row["righteye_x"],row["righteye_y"]))
 	image.show()
 	raw_input("Press Return ")
 cursor.close()
