1b1832f9703b9cd42fe436c16e827795cf8ae983
Lemy Danger <git@lemydanger.eu>
2015-02-11 22:02:03 +0100

    test

--- backend/sqlfaces.py
+++ backend/sqlfaces.py
@@ -3,7 +3,7 @@
 import Image
 import mysql.connector
 import sqlconnection
-import cStringIO as StringIO
+import cStringIO
 import newfaces
 
 cnx = sqlconnection.connecttodb()
@@ -13,14 +13,14 @@ id = 17
 query = ("SELECT id, lefteye_x, lefteye_y, righteye_x, righteye_y, imgdata, imgtype, userid FROM images WHERE id = (%s)")
 cursor.execute(query, (id,))
 for row in cursor:
-	file_like = StringIO(row["imgdata"])
+	file_like = cStringIO.StringIO(row["imgdata"])
 	image = Image.open(file_like)
 	(width, height) = image.size
 	eye_left=(row["lefteye_x"]*width,row["lefteye_y"]*height)
 	eye_right=(row["righteye_x"]*width,row["righteye_y"]*height)
 	image = newfaces.CropFace( image, eye_left, eye_right)
 	query = ("INSERT INTO faces (id, imgdata, imgtype, userid) VALUES (%(id)s, %(imgdata)s, %(imgtype)s, %(userid)s)")
-	im_str = StringIO(image)
+	im_str = cStringIO.StringIO(image)
 	cursor.execute(query, (row["id"], im_str, row["imgtype"], row["userid"]))
 cursor.close()
 cnx.close()
