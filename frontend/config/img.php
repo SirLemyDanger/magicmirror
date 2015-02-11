f37561fc1b932c97fe3835f675847e2b84aeac40
Lemy Danger <git@lemydanger.eu>
2015-02-11 23:50:49 +0100

    img filename

--- frontend/config/img.php
+++ frontend/config/img.php
@@ -33,7 +33,7 @@ if(!$result){
 }else{
     $row = $result->fetch_array(MYSQLI_ASSOC);
     $filename = $id . $type;
-    caching_headers($id,$row['last_modified']);
+    caching_headers($filename,$row['last_modified']);
     header("Content-type: {$row['imgtype']}");
     echo $row['imgdata'];
 }
