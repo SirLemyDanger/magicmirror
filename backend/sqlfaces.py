948a3e1babb3bcb5d627f6a6bb8b04963b876b4a
Lemy Danger <git@lemydanger.eu>
2015-02-11 23:10:56 +0100

    code zu funtion

--- backend/sqlfaces.py
+++ backend/sqlfaces.py
@@ -6,26 +6,29 @@ import sqlconnection
 import cStringIO
 import newfaces
 
-cnx = sqlconnection.connecttodb()
+def imageToFace(*ids):
+	cnx = sqlconnection.connecttodb()
+	cursor = DBconnection.cursor(dictionary=True)
+	for id in ids:
+		query = ("SELECT lefteye_x, lefteye_y, righteye_x, righteye_y, imgdata, userid FROM images WHERE id = (%s)")
+		cursor.execute(query, (id,))
+		for row in cursor:
+			file_like = cStringIO.StringIO(row["imgdata"])
+			image = Image.open(file_like)
+			(width, height) = image.size
+			eye_left=(row["lefteye_x"]*width,row["lefteye_y"]*height)
+			eye_right=(row["righteye_x"]*width,row["righteye_y"]*height)
+			image = newfaces.CropFace( image, eye_left, eye_right)
+			query = ("REPLACE INTO faces (id, imgdata, imgtype, userid) VALUES (%s, %s, %s, %s)")
+			output = cStringIO.StringIO()
+			image.save(output, format="PNG")
+			imagetype = "image/png"
+			data = output.getvalue()
+			cursor.execute(query, (id, data, imgtype, row["userid"]))
+			cnx.commit()
+	cursor.close()
+	cnx.close()
 
-cursor = cnx.cursor(dictionary=True)
-id = 17
-query = ("SELECT id, lefteye_x, lefteye_y, righteye_x, righteye_y, imgdata, imgtype, userid FROM images WHERE id = (%s)")
-cursor.execute(query, (id,))
-for row in cursor:
-	file_like = cStringIO.StringIO(row["imgdata"])
-	image = Image.open(file_like)
-	(width, height) = image.size
-	eye_left=(row["lefteye_x"]*width,row["lefteye_y"]*height)
-	eye_right=(row["righteye_x"]*width,row["righteye_y"]*height)
-	image = newfaces.CropFace( image, eye_left, eye_right)
-	query = ("INSERT INTO faces (id, imgdata, imgtype, userid) VALUES (%s, %s, %s, %s)")
+if __name__ == "__main__":
 	
-	output = cStringIO.StringIO()
-	image.save(output, format="GIF")
-	data = output.getvalue()
-	cursor.execute(query, (row["id"], data, row["imgtype"], row["userid"]))
-	cnx.commit()
-cursor.close()
-cnx.close()
-
+	imageToFace(17,18,19,20)
