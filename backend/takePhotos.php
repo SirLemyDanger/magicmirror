c7d4657e94b362d21b91dd03f960809ca570c23c
Lemy Danger <git@lemydanger.eu>
2015-02-22 23:18:32 +0100

    rpi

--- backend/takePhotos.php
+++ backend/takePhotos.php
@@ -4,14 +4,14 @@ $method = filter_input(INPUT_POST, "method");
 $photocounter = filter_input(INPUT_POST, "photocounter");
 $userid = filter_input(INPUT_POST, "userid");
 
-if ( $method == "photo" && $photocounter != False && $userid != False)
+if ( $method == "photo" && is_numeric($photocounter) && $userid != False)
 {
-	echo is_int($photocounter);
+	$photocounter = intval($photocounter);
+	
 }
 else{
-	
-header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
-echo "Bad Request";
+	header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+	echo "Bad Request";
 }
 
     // $pipe ="/tmp/pipe_faceRec";
