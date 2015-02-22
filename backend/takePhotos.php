363c6c0df2019c3234d8c859d4022d5ea04bbbda
Lemy Danger <git@lemydanger.eu>
2015-02-22 23:35:13 +0100

    take foto connector

--- backend/takePhotos.php
+++ backend/takePhotos.php
@@ -7,6 +7,34 @@ $userid = filter_input(INPUT_POST, "userid");
 if ( $method == "photo" && is_numeric($photocounter) && $userid != False)
 {
 	$photocounter = intval($photocounter);
+	$transfer = array(
+					'method' => $method,
+					'photocounter' => $photocounter,
+					'userid' => $userid
+	)
+	$jsonTransfer = json_encode($transfer);
+ 
+    $pipe_out ="/tmp/pipe_query";
+    if(!file_exists($pipe_out)){
+        umask(0);
+        posix_mkfifo( $pipe, 0666 );
+    }
+    $handle = fopen($pipe_out, "w");
+	fwrite($handle, $jsonTransfer);
+    fclose($handle);
+	
+	$pipe_in ="pipe_fotoout";
+    if(!file_exists($pipe)){
+        umask(0);
+        posix_mkfifo( $pipe, 0666 );
+    }
+    $data = "";
+    $handle = fopen($pipe_in, "r");
+    while ($input = fread($handle, 1024)) {
+            $data .= $input;
+    }
+    fclose($handle);
+    echo $data;
 	
 }
 else{
