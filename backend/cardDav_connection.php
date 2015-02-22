7d31c33d4e367da77b62a6a955b0a29af2b8b4ba
Lemy Danger <git@lemydanger.eu>
2015-02-22 20:53:37 +0100

    ignore

---
+++ backend/cardDav_connection.php
@@ -0,0 +1,7 @@
+<?php
+$settings = array(
+    'baseUri' => "https://malte.single-point-of-failure.com:28966",
+    'userName' => 'mirror',
+    'password' => 'koenig\magicmirror',
+    //'proxy' => 'locahost:8888',
+);
