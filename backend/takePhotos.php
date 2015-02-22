2d3cf45344025b5081b8c81a41a0fdba35cfdf4b
Lemy Danger <git@lemydanger.eu>
2015-02-23 00:15:18 +0100

    fix

--- backend/takePhotos.php
+++ backend/takePhotos.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-Type: text/plain; charset=utf-8');
+header('Content-Type: application/json; charset=utf-8');
 $method = filter_input(INPUT_POST, "method");
 $photocounter = filter_input(INPUT_POST, "photocounter");
 $userid = filter_input(INPUT_POST, "userid");
@@ -34,26 +34,9 @@ if ( $method == "photo" && is_numeric($photocounter) && $userid != False)
             $data .= $input;
     }
     fclose($handle);
-    #echo $data;
-	echo $jsonTransfer;
+    echo $data;
 }
 else{
 	header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
 	echo "Bad Request";
 }
-
-    // $pipe ="/tmp/pipe_faceRec";
-    // if(!file_exists($pipe)){
-        // umask(0);
-        // posix_mkfifo( $pipe, 0666 );
-    // }
-    // $data = "";
-    // $handle = fopen("/tmp/pipe_faceRec", "r");
-    // while ($input = fread($handle, 1024)) {
-            // $data .= $input;
-    // }
-    // fclose($handle);
-    // echo $data;
-    
-
-
