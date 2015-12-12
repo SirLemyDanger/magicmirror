f607b1e9655382b84915aa62f3d751c8cf5bc078
SirLemyDanger <github@lemydanger.eu>
2015-12-12 17:46:36 +0100

    debug

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -94,11 +94,11 @@ jQuery( document ).ready(function() {
 						return;
 				}
 			numOfFaces = data.length;
-			//$( "#name" ).html( "" )
+			$( "#name" ).html( "" )
 			for (var i = 0; i < numOfFaces; i++)
 				if (data[i].typ == "sex")
 				{
-					//$( "#name" ).append("Geschlecht: "+ data[i].prediction+ " confidence: " +data[i].confidence+ "<br>");
+					$( "#name" ).append("Geschlecht: "+ data[i].prediction+ " confidence: " +data[i].confidence+ "<br>");
 				}
 				else if ( data[i].typ == "person")
 				{
@@ -293,11 +293,11 @@ jQuery( document ).ready(function() {
 						if ((0 < diff) && (diff <= 105000)){
                             speedup = true;
                         }
-						if (diff > (22*60*60*1000)){//22hours
-                            birthday.setHours(now.getHours());
-							birthday.setMinutes(now.getMinutes());
-							birthday.setSeconds(now.getSeconds());
-                        }
+						// if (diff > (22*60*60*1000)){//22hours
+                            // birthday.setHours(now.getHours());
+							// birthday.setMinutes(now.getMinutes());
+							// birthday.setSeconds(now.getSeconds());
+                        // }
                     }
                     var row = $('<tr />').css('opacity', opacity);
                     row.append($('<td/>').addClass('name').html(birthdays.bdays[i][0]));
