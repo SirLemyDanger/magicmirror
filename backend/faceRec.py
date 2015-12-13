5301b5f7fc112c3920584f99ad5b38c1bf5de2c3
SirLemyDanger <github@lemydanger.eu>
2015-12-13 14:41:26 +0100

    bug

--- backend/faceRec.py
+++ backend/faceRec.py
@@ -85,7 +85,7 @@ def faceRec():
 				output.truncate(0)
 				camera.capture(output, 'rgb', resize=pictureSize, use_video_port=True)
 				gray = cv2.cvtColor(output.array, cv2.COLOR_RGB2GRAY )
-				cv2.inshow("test",gray)
+				cv2.imshow("test",gray)
 				waitKey(0)
 				faces = face_cascade.detectMultiScale(image=gray, scaleFactor=1.1, minNeighbors=5, minSize=(40,40))	
 				json_transfer = "["
