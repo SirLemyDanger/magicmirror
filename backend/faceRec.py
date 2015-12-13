b0416b2c3a1c937e734b7f3610af015106f1b780
SirLemyDanger <github@lemydanger.eu>
2015-12-13 15:19:52 +0100

    destroyallwindow

--- backend/faceRec.py
+++ backend/faceRec.py
@@ -87,6 +87,7 @@ def faceRec():
 				gray = cv2.cvtColor(output.array, cv2.COLOR_RGB2GRAY )
 				cv2.imshow("test",gray)
 				cv2.waitKey(0)
+				cv2.destroyAllWindows()
 				faces = face_cascade.detectMultiScale(image=gray, scaleFactor=1.1, minNeighbors=5, minSize=(40,40))	
 				json_transfer = "["
 				jsonObjCounter = 0
